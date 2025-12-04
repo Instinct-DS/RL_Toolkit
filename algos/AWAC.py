967470ac4470ecfef68a011d58f71dfa58ddf548
Instinct-DS <emailtojjoel@gmail.com>
2025-12-04 14:18:51 +0530

    added trainloop

--- algos/AWAC.py
+++ algos/AWAC.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import random
 from time import time
+from tqdm import tqdm
 import torch.optim as optim
 from common.replaybuffer import NStepReplayBuffer
 from common.logger import MLFlowLogger
@@ -44,6 +45,8 @@ class AWAC:
             q_update_period = 1,
             use_automatic_entropy_tuning = False,
             logger_name="mlflow",
+            experiment_name = "",
+            run_name = "",
             device="cuda" if torch.cuda.is_available() else "cpu"   
     ):
         
@@ -113,6 +116,10 @@ class AWAC:
 
         self.replay_buffer = NStepReplayBuffer(state_dim, action_dim, buffer_size, batch_size, n_step, gamma, n_envs)
 
+        # Logger Init #
+        if logger_name == "mlflow":
+            self.logger = MLFlowLogger(uri="http://127.0.0.1:5000", experiment_name=experiment_name, run_name=run_name)
+
         # Log hyperparams #
         self.hparams = {
             "state_dim": self.state_dim,
@@ -145,7 +152,7 @@ class AWAC:
 
         return policy_loss, mse_loss
 
-    def train(self):
+    def _train(self):
         batch_m, terminal_flag_m = self.replay_buffer.sample()
         states_m, actions_b_m, rewards_b_m, next_states_m, terminations_m, truncations_m = batch_m
 
@@ -221,10 +228,146 @@ class AWAC:
             POLICY_LOSS.backward()
             self.actor_optimizer.step()
 
-        # SOFT UPDATES #
-        SoftUpdate(self.target_critic1, self.critic1, self.tau)
-        SoftUpdate(self.target_critic2, self.critic2, self.tau)
+        if self._n_train_steps_total % self.target_update_interval == 0:
+            # SOFT UPDATES #
+            SoftUpdate(self.target_critic1, self.critic1, self.tau)
+            SoftUpdate(self.target_critic2, self.critic2, self.tau)
 
         self._n_train_steps_total += 1
 
         return POLICY_LOSS.item(), QF1_LOSS.item(), QF2_LOSS.item(), ALPHA_LOSS.item(), alpha  
+    
+    def train_loop_awac(
+            self,
+            env,
+            train_offline_steps=10_000, 
+            train_online_steps=100_000,
+            progress_bar = True,
+            log_interval = 10,
+            verbose = 1,
+    ):
+        self.train_offline_steps = train_offline_steps
+        self.train_online_steps = train_online_steps
+        self.log_interval = log_interval
+        log_interval_offline = self.log_interval
+        self.verbose = verbose
+        self.progress_bar = progress_bar
+
+        progress_bar_1 = tqdm(total=self.train_offline_steps, desc="Offline Training Steps")
+        log_interval_offline = self.log_interval
+        actor_offline_loss = deque(maxlen=log_interval_offline)
+        critic1_offline_loss = deque(maxlen=log_interval_offline)
+        critic2_offline_loss = deque(maxlen=log_interval_offline)
+        alpha_offline_loss = deque(maxlen=log_interval_offline)
+        alpha_offline = deque(maxlen=log_interval_offline)
+
+
+
+        self.logger.start()
+        self.logger._log_params(self.hparams)
+
+        while self._n_train_steps_total <= self.train_offline_steps:
+            policy_loss, critic1_loss, critic2_loss, alpha_loss, alpha = self._train()
+            actor_offline_loss.append(policy_loss)
+            critic1_offline_loss.append(critic1_loss)
+            critic2_offline_loss.append(critic2_loss)
+            alpha_offline_loss.append(alpha_loss)
+            alpha_offline.append(alpha)
+            
+            if self._n_train_steps_total % log_interval_offline == 0:
+                self.logger.log_metric("actor_loss", np.mean(actor_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("critic1_loss", np.mean(critic1_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("critic2_loss", np.mean(critic2_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("alpha_loss", np.mean(alpha_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("alpha", np.mean(alpha_offline), step=self.timesteps_ran)
+
+            progress_bar_1.update(1)
+        progress_bar_1.close()
+
+        # Reset for online training #
+        self._n_train_steps_total = 0
+        progress_bar_2 = tqdm(total=self.total_offline_gradient_steps, desc="Online Training Steps")
+        obs, _ = env.reset()
+        episode_start = np.zeros(env.num_envs, dtype=bool)
+        episode_rewards = np.zeros(shape=(env.num_envs,))
+        episode_lengths = np.zeros(shape=(env.num_envs,))
+        self.logger_count = 1
+        self.timesteps_ran = 0
+        while self.timesteps_ran <= self.train_online_steps:
+            with torch.no_grad():
+                actions = self.actor.sample(obs)
+            # Environment step
+            next_obs, rewards, terminations, truncations, infos = env.step(actions)
+            dones = np.logical_or(terminations, truncations)
+
+            # Add transitions to replay buffer (flatten batch items)
+            for i in range(env.num_envs):
+                if not episode_start[i]:
+                    self.replay_buffer.add(
+                        i,
+                        obs[i],
+                        actions[i],
+                        rewards[i],
+                        terminations[i],
+                        truncations[i],
+                        next_obs[i]
+                    )
+                else:
+                    self.ep_step_count += 1
+                    self.logger_count += 1
+                    self.replay_buffer.clear_n_step_buffer(i)
+                    self.ep_lengths.append(episode_lengths[i])
+                    self.ep_rewards.append(episode_rewards[i])
+                    episode_rewards[i], episode_lengths[i] = 0, -1
+
+
+            # Track episode rewards and lengths
+            episode_rewards += rewards
+            episode_lengths += 1
+            self.timesteps_ran += env.num_envs
+            obs = next_obs
+            episode_start = dones
+            
+            if self._n_train_steps_total % self.train_freq == 0:
+                for _ in range(self.gradient_steps):
+                    policy_loss, critic1_loss, critic2_loss, alpha_loss, alpha = self._train()
+                    actor_offline_loss.append(policy_loss)
+                    critic1_offline_loss.append(critic1_loss)
+                    critic2_offline_loss.append(critic2_loss)
+                    alpha_offline_loss.append(alpha_loss)
+                    alpha_offline.append(alpha)
+
+            if self.logger_count % self.log_interval == 0:
+                # Compute mean metrics over interval
+                mean_ep_length = np.mean(self.ep_lengths) if len(self.ep_lengths) > 0 else 0
+                mean_ep_reward = np.mean(self.ep_rewards) if len(self.ep_rewards) > 0 else 0
+                elapsed_time = time() - self.start_time
+                fps = self.timesteps_ran / elapsed_time if elapsed_time > 0 else 0
+
+                # Log metrics to MLflow
+                self.logger.log_metric("mean_episode_length", mean_ep_length, step=self.timesteps_ran)
+                self.logger.log_metric("mean_episode_reward", mean_ep_reward, step=self.timesteps_ran)
+                self.logger.log_metric("frames_per_second", fps, step=self.timesteps_ran)
+                self.logger.log_metric("actor_loss", np.mean(actor_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("critic1_loss", np.mean(critic1_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("critic2_loss", np.mean(critic2_offline_loss), step=self.timesteps_ran)
+                self.logger.log_metric("entropy_coefficient", np.mean(alpha_offline), step=self.timesteps_ran)
+                self.logger.log_metric("alpha_loss", np.mean(alpha_offline_loss), step=self.timesteps_ran)
+
+
+                if verbose != 0:
+                    tqdm.write("\n" + "="*90)
+                    tqdm.write(f" Step: {self.timesteps_ran:<8d} | MeanEpLen: {mean_ep_length:.2f} | MeanEpRew: {mean_ep_reward:.2f} | FPS: {fps:.0f}")
+                    tqdm.write(f" Actor Loss: {np.mean(actor_offline_loss):.4f} | Critic Loss: {min(np.mean(critic2_offline_loss), np.mean(critic1_offline_loss)):.4f} | Alpha: {np.mean(alpha_offline):.4f}")
+                    tqdm.write("="*90)
+                
+                self.logger_count = 1
+
+            progress_bar_2.update(env.num_envs)
+        progress_bar_2.close()
+
+
+
+
+
+
