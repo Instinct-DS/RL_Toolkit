ac0046de753bbf6e769d8541e91289f07f77c840
Instinct-DS <emailtojjoel@gmail.com>
2025-12-12 19:51:24 +0530

    added interpolating function Utils_OCP

--- common/utils.py
+++ common/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pickle
 from collections import deque
 from tqdm import tqdm
+from scipy.integrate import BarycentricInterpolator
 
 # Find atanh #
 def atanh(x, eps=1e-3):
@@ -20,32 +21,57 @@ def SoftUpdate(target_network, source_network, tau=0.005):
             (1 - tau) * target_param.data + tau * source_param.data
         )
 
+# Get Interpolator #
+def getInterpolators(result, nds):
+    n = len(nds)
+    Z = result.x
+    states = np.reshape(Z[:6*n], shape=(n,6), order='F')
+    ctrl = np.reshape(Z[6*n:8*n], shape=(n,2), order="F")
+    tf = Z[-1]
+
+    x_i = states[:, 0]
+    z_i = states[:, 1]
+    u_i = states[:, 2]
+    w_i = states[:, 3]
+    omg_i = states[:, 4]
+    lmbi = states[:, 5]
+    tht0 = ctrl[:, 0]
+    thtP = ctrl[:, 1]
+
+    x_int = BarycentricInterpolator(nds, x_i)
+    z_int = BarycentricInterpolator(nds, z_i)
+    u_int = BarycentricInterpolator(nds, u_i)
+    w_int = BarycentricInterpolator(nds, w_i)
+    omg_int = BarycentricInterpolator(nds, omg_i)
+    lmbi_int = BarycentricInterpolator(nds, lmbi)
+    tht0_int = BarycentricInterpolator(nds, tht0)
+    thtP_int = BarycentricInterpolator(nds, thtP)
+
+    return x_int, z_int, u_int, w_int, omg_int, lmbi_int, tht0_int, thtP_int
+
+
 # Load expert trajectories into replay buffer #
-def load_demo_trajectories(replay_buffer, demo_file, demo_env, demo_ocp, n_step=1, gamma=0.999):
+def load_demo_trajectories(replay_buffer, demo_file, demo_env, nds, gamma=0.999):
     demo_file = demo_file
     with open(demo_file, "rb") as file:
         TRAJ = pickle.load(file)
 
     count_land, count_crash = 0, 0 
     test_env = demo_env
-    buff = deque(maxlen=n_step)
 
     # Loading the trajectories #
     for key in tqdm(TRAJ.keys(), desc="Loading Trajectories", unit="trj"):
         tr_ = TRAJ[key]
         result = tr_["solution_vector"]
 
-        x_int, z_int, u_int, w_int, omg_int, lmbi_int, tht0_int, thtP_int = demo_ocp.getInterpolators(result)
+        x_int, z_int, u_int, w_int, omg_int, lmbi_int, tht0_int, thtP_int = getInterpolators(result, nds)
         tf = result[-1]
 
         success = False
         for eps in (0.0, -0.025, 0.025, -0.05, 0.05):
-            x_int, z_int, u_int, w_int, omg_int, lmbi_int, tht0_int, thtP_int = demo_ocp.getInterpolators(result)
+            x_int, z_int, u_int, w_int, omg_int, lmbi_int, tht0_int, thtP_int = getInterpolators(result, nds)
             init = (x_int(-1), z_int(-1)+eps, u_int(-1), w_int(-1), omg_int(-1), tht0_int(-1), thtP_int(-1), lmbi_int(-1))
             action_int = (tht0_int, thtP_int)
-
-            test_env.dt=0.02
-            test_env.rk4_dt=0.01
             sim_traj = test_env._similate_trajectory(init, action_int, tf)
 
             if sim_traj[-1][3] and sim_traj[-1][-1]["constraints"][0][2]:
@@ -57,23 +83,11 @@ def load_demo_trajectories(replay_buffer, demo_file, demo_env, demo_ocp, n_step=
             for i in range(len(sim_traj)):
                 experience = sim_traj[i]
                 state, action, reward, termination, truncation, next_state, info = experience
-                replay_buffer.add(0, state, action, reward, termination, truncation, next_state)
-                # Offline Buffer add
-                experience = sim_traj[-i-1]
-                state, action, reward, termination, truncation, next_state, info = experience
-                if i == 0:
-                    value_fn = 0
-                    n_step_value_fn = 0
-                else:
-                    value_fn = buff[-1][0] + gamma*buff[-1][1]
-                    n_step_value_fn = 0
-                    for j in range(len(buff)):
-                        n_step_value_fn += buff[-j-1][0]*gamma**(j)
-                    n_step_value_fn += buff[0][1]*gamma**(len(buff))
-                buff.append((reward, value_fn))
-                replay_buffer.add(0, state, action, reward, termination, truncation, next_state)
+                for buffer_i in replay_buffer:
+                    buffer_i.add(0, state, action, reward, termination, truncation, next_state)
 
-            replay_buffer.clear_n_step_buffer(0)
+            for buffer_i in replay_buffer:
+                buffer_i.clear_n_step_buffer(0)
         else:
             count_crash += 1
             # tqdm.write(f"Failed : {key}")
